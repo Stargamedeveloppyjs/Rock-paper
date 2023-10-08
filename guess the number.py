4a1dcd05799f68a7ed8737af50e077596c7363f9
Stargamedeveloppyjs <147295822+Stargamedeveloppyjs@users.noreply.github.com>
2023-10-08 10:37:59 +0530

    Add files via upload

---
+++ guess the number.py
@@ -0,0 +1,29 @@
+import random
+
+def play_game():
+    choices = ["rock", "paper", "scissors"]
+
+    while True:
+        user_choice = input("Enter your choice (rock, paper, scissors): ")
+        if user_choice not in choices:
+            print("Invalid choice. Please choose rock, paper, or scissors.")
+            continue
+
+        computer_choice = random.choice(choices)
+        print(f"Computer chose {computer_choice}.")
+
+        if user_choice == computer_choice:
+            print("It's a tie!")
+        elif (user_choice == "rock" and computer_choice == "scissors") or \
+             (user_choice == "paper" and computer_choice == "rock") or \
+             (user_choice == "scissors" and computer_choice == "paper"):
+            print("You win!")
+        else:
+            print("Computer wins!")
+
+        play_again = input("Play again? (yes/no): ")
+        if play_again.lower() != "yes":
+            break
+
+if __name__ == "__main__":
+    play_game()
